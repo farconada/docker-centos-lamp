b7d6488093a1dbfca3ec3be98c1dcc016f3ae027
root <root@sorano.riojasalud.es>
2015-11-06 09:29:00 +0100

    bower and composer

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,7 +1,7 @@
 
 include apache
 class{'php':
-	 php_packages => ['php56w', 'php56w-mysqlnd', 'php56w-curl', 'php56w-gd', 'php56w-json', 'php56w-mhash', 'php56w-pecl-apcu', 'php56w-readline']
+	 php_packages => ['php56w', 'php56w-mysqlnd', 'php56w-curl', 'php56w-gd', 'php56w-json', 'php56w-mhash', 'php56w-pecl-apcu', 'php56w-readline', 'php56w-pdo.x86_64', 'php56w-xml']
 }
 
 file {'/etc/service/apache':
@@ -13,6 +13,22 @@ file {'/etc/service/apache/run':
 	mode 		=> '777',
 	source 		=> 'file:///puppet/files/runit/service/apache-service'
 }
+
+
+file {'/usr/local/bin/composer':
+        ensure          => 'present',
+        mode            => '777',
+        source          => 'file:///puppet/files/composer.phar',
+}
+
+class { 'nodejs': }
+
+package { 'bower':
+	    ensure => present,
+	    provider => 'npm',
+}
+
+Class['nodejs'] -> Package['bower']
 
 File['/etc/service/apache/'] -> File['/etc/service/apache/run']
 
